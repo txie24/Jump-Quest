62327b2bd0e4d4d4fedf51849fd69dcd2bbb1c8d
HengyangYe <heye@ucsc.edu>
2024-06-11 21:16:57 -0700

    update

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -17,7 +17,7 @@ class Platformer extends Phaser.Scene {
 
     init() {
         this.ACCELERATION = 200;
-        this.DRAG = 1000;
+        this.DRAG = 500;
         this.physics.world.gravity.y = 1500;
         this.PARTICLE_VELOCITY = 50;
         this.SCALE = 2.0;
